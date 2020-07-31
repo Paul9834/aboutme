7cc40f135e57149ca4c16913bbcec5055eacae4d
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-07-31 15:57:35 -0500

    Initial commit

---
+++ app/src/main/java/com/paul9834/aboutme/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.paul9834.aboutme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
