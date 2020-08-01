41b908e5c7b042c0a94a20e6b5279fe684352395
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-07-31 20:54:22 -0500

    Implementación de Bottom Navigation y Controller.

---
+++ app/src/main/java/com/paul9834/aboutme/ui/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.paul9834.aboutme.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.snackbar.Snackbar
+import com.paul9834.aboutme.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+
+        bottom_navigation.setupWithNavController(navController)
+
+        floating_action_button.setOnClickListener{
+            Snackbar.make(floating_action_button, "Me has presionado <3", Snackbar.LENGTH_SHORT).show()
+        }
+
+    }
+
+}
